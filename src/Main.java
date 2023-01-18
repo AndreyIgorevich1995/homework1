ca1fb9661dee3c84ead1bae3805c4016ff9b3e65
Puzyrnikow Andrey <daf1>
2023-01-18 13:59:06 +0400

    modified int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] resultsManagers={30, 13, 83, 101, 25, 90};
+        long[] resultsManagers={30, 13, 83, 101, 25, 90};
         SalesManager Petia=new SalesManager(resultsManagers);
         System.out.println(Petia.max());
     }
