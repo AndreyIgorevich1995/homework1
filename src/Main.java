a20d4cac807656bfd50b02d3714ebc3cdda445b5
Puzyrnikow Andrey <daf1>
2023-01-18 14:22:14 +0400

    add stat

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        long[] resultsManagers={30, 13, 83, 101, 25, 90};
+        int[] resultsManagers={30, 13, 83, 101, 25, 90};
         SalesManager Petia=new SalesManager(resultsManagers);
         System.out.println(Petia.max());
+        System.out.println(Petia.min());
+        System.out.println(Petia.average());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,17 +1,35 @@
 public class SalesManager {
-    protected long[] sales;
+    protected int[] sales;
 
-    public SalesManager(long[] sales) {
+    public SalesManager(int[] sales) {
         this.sales = sales;
     }
 
-    public long max() {
-        long max = -1;
-        for (long sale : sales) {
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
     }
+
+    public int min() {
+        int min = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < min) {
+                min = sales[i];
+            }
+        }
+        return min;
+    }
+    public int average(){
+        int average=0;
+        for (int i = 0; i < sales.length; i++) {
+            average+=sales[i];
+        }
+        return (average-(min()+max()))/(sales.length-2);
+    }
+
 }
